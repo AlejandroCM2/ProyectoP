e309ca5d90a7d9631ae6524b6624151484343b82
AlejandroCM <alejandromonsalve2013@gmail.com>
2023-08-08 13:15:09 -0500

    punto2

---
+++ Ejercicio2.py
@@ -0,0 +1,15 @@
+class Punto:
+    def __init__(self, x: int, y: int):
+        self.x: int = x
+        self.y: int = y
+
+if __name__ == "__main__":
+   p1:Punto = Punto (2,5)
+   p2:Punto = Punto (4,7)
+print("Punto1",p1.x,p1.y)
+print("Punto2",p2.x,p2.y)
+
+
+
+
+
